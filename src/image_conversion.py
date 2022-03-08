14cffb3e0a6e118ff84f9e7d9a23db9932990fee
Dennis Gerritsen <dvw.gerritsen@student.han.nl>
2022-03-08 12:31:09 +0100

    Update image conversion

--- src/image_conversion.py
+++ src/image_conversion.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pandas as pd
 from skimage import io
 from skimage.io import imread
+from skimage.transform import resize
 from tqdm import tqdm
 
 path = Path("../data/annotations.csv")
@@ -16,9 +17,11 @@ for img_name in tqdm(df['image_name']):
     # defining the image path
     image_path = '../data/images/' + str(img_name)
     # reading the image
-    img = imread(image_path, as_gray=True)
+    img = imread(image_path)
+    #image_resized = resize(img, (400, 400),anti_aliasing=True)
+    #, as_gray=True)
     # normalizing the pixel values
-    img /= 255.0
+    #img /= 255.0
     # converting the type of pixel to float 32
     img = img.astype('float32')
     # appending the image into the list
