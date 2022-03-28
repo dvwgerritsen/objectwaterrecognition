b1cb8d71890786acb942b922dd0b2a067940062b
Koen Habets <koenhabets1@gmail.com>
2022-03-28 17:43:50 +0200

    Predict images in web application

--- src/app.py
+++ src/app.py
@@ -1,32 +1,33 @@
 import os
 from flask import Flask, render_template, request, redirect, send_from_directory
 from werkzeug.utils import secure_filename
+from predict_nieuw import predict
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    result = ''
     image = ''
+    error = ''
     if request.method == 'POST':
         if 'file' not in request.files:
             print('No file part')
             return redirect(request.url)
 
         file = request.files['file']
-        if file and file.filename.rsplit('.', 1)[1].lower() == 'jpg':
+        file_extension = file.filename.rsplit('.', 1)[1].lower()
+        if file and (file_extension == 'jpg' or file_extension == 'jpeg' or file_extension == 'png'):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(filename))
-            image = filename
-            # todo do something with the uploaded image and set the result
+            file.save(os.path.join("upload/", filename))
+            image = predict("upload/" + filename, False)
         else:
-            print('Only jpg is supported')
+            print('Only jpg or png is supported')
+            error = 'Het bestandstype moet jpg of png zijn.'
 
-    return render_template("index.html", result=result, image=image)
+    return render_template("index.html", image=image, error=error)
 
 
-# todo files need to be uploaded to a separate directory
-@app.route('/<path:path>')
+@app.route('/processed-image/<path:path>')
 def send_report(path):
-    return send_from_directory('', path)
+    return send_from_directory('predicted_images', path)
